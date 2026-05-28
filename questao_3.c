c350196144059a63d4b582084de9ea4c47a94777
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 11:55:00 -0400

    Update questao_3.c

--- questao_3.c
+++ questao_3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+	int n1;
+	int *troca;
+	
+	printf("Digite o valor da variável:\n");
+	scanf("%d", &n1);
+	
+	troca = &n1;
+
+	printf ("O valor da variável é:%d\n", n1);
+    printf ("O endereço da variável é: %p\n", &n1);
+    printf ("O valo do ponteiro é: %p\n", troca);
+    printf ("O valor via ponteiro é: %d\n", *troca);
+
+	return 0;
+}
