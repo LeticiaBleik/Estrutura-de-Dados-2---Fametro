f9b7109914ed837a05ae8046af048f658fb2ce32
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 12:37:51 -0400

    Update questao_5.c

--- questao_5.c
+++ questao_5.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+void copiarString(char *origem, char *destino) {
+	while (*origem != '\0') {
+		*destino = * origem;
+		origem ++;
+		destino ++;
+	}
+	*destino = '\0';
+
+}
+
+int main () {
+	char plv1[100], plv2[100];
+
+	printf("Digite uma palavra:\n");
+	scanf ("%s", &plv1);
+
+	copiarString (plv1, plv2);
+
+	printf ("A palavra original e a palavra copiada são:%s e %s\n", plv1, plv2);
+
+	return 0;
+}
