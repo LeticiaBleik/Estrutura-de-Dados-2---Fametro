6740111f78709f7db0b6e62432c355bd75207d4d
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 10:54:14 -0400

    Update questao_1.c

--- questao_1.c
+++ questao_1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+void ponteiros(int *a, int *b){
+
+    *a = 10;
+    *b = 20;    
+}
+
+int main (){
+    int n1, n2;
+  
+    printf ("Digite dois numeros:\n");
+    scanf ("%d %d", &n1, &n2);
+    
+    ponteiros (&n1, &n2);
+    
+    printf ("Os valores via ponteiro são %d e %d", n1, n2);
+
+return 0;
+}
