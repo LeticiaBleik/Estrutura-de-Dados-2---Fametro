4932c7a0a43d589abf61e73c8103931c8be095bb
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 15:18:45 -0400

    Update questao_7.c

--- questao_7.c
+++ questao_7.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+
+void inversao (char *str) {
+
+	char * inicio = str;
+	char * fim = str;
+
+	while (*fim != '\0') {
+		fim ++;
+	}
+	fim --;
+
+	while (inicio<fim) {
+
+		char temporaria = *inicio;
+		*inicio = *fim;
+		*fim = temporaria;
+
+		inicio ++;
+		fim --;
+	}
+}
+
+int main () {
+	char palavra[100];
+
+	printf ("Digite uma palavra:\n");
+	scanf ("%s", palavra);
+
+	inversao (palavra);
+
+	printf ("A palavra fica:%s", palavra);
+
+	return 0;
+
+}
+
 
