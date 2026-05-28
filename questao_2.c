3b04583d5ab12874a1174243d60609a9ccf10397
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 11:19:48 -0400

    Update questao_2.c

--- questao_2.c
+++ questao_2.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+void encontrarMaiorMenor(int*array, int tamanho, int *maior, int *menor) {
+
+	*maior = array[0];
+	*menor = array[0];
+
+	for (int i = 1; i < tamanho; i ++) {
+
+		if (array[i] > *maior) {
+			*maior = array[i];
+		} else if(array[i] < *menor) {
+			*menor = array[i];
+		}
+	}
+}
+
+int main() {
+	int numeros[5], i;
+	int maior, menor;
+
+	printf ("Digite 5 números:\n");
+
+	for (i = 0; i < 5; i++) {
+		scanf ("%d", &numeros[i]);
+
+	}
+
+	encontrarMaiorMenor (numeros, 5, &maior, &menor);
+
+	printf ("O maior número é %d, e o menor é %d.", maior, menor);
+
+	return 0;
+}
