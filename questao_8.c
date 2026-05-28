0e77de9b99a680cfce54cbb2dc8acc16cbf90355
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 16:17:01 -0400

    Update questao_8.c

--- questao_8.c
+++ questao_8.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+void calculadora (float a, float b, float * soma, float * sub, float *multi, float *divisao) {
+
+	*soma = a + b;
+	*sub = a - b;
+	*multi = a * b;
+	*divisao = a / b;
+
+}
+
+int main () {
+	float n1, n2, soma, sub, multi, divisao;
+
+	printf ("Digite dois números:\n");
+	scanf ("%f %f", &n1, &n2);
+
+	calculadora (n1, n2, &soma, &sub, &multi, &divisao);
+
+	printf("RESULTADO\n%.1f + %.1f = %.1f\n%.1f - %.1f = %.1f\n%.1f * %.1f = %.1f\n%.1f / %.1f = %.2f", n1,n2, soma, n1, n2, sub, n1, n2, multi, n1, n2, divisao);
+
+	return 0;
+}
