27b528808de65127b454135a88ad2618474acf59
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 13:41:48 -0400

    Update questao_6.c

--- questao_6.c
+++ questao_6.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+ void contarVogaisConsoantes(char *str, int *vogais, int*consoantes){
+     
+     *vogais = 0;
+     *consoantes = 0;
+     
+     while (*str != '\0'){
+         if (*str == 'a' || *str =='e'|| *str == 'i'|| *str == 'o'|| *str == 'u'||
+         *str == 'A'|| *str == 'E'|| *str == 'I'|| *str == 'O'|| *str == 'U'){
+         (*vogais)++;
+         } else {
+            (*consoantes)++;
+         }
+         str ++;
+         }
+     }
+ int main (){
+     char palavra[100];
+     int consoantes, vogais;
+     
+     printf ("Digite uma palavra:\n");
+     scanf ("%s", palavra);
+     
+     contarVogaisConsoantes (palavra, &vogais, &consoantes);
+     
+     printf ("Palavra: %s | Vogais: %d | Consonates: %d", palavra, vogais, consoantes);
+     
+     return 0;
+     
+ }
+
+ 
