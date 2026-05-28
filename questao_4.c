9bd7176af7a45eae4792ab61e42272772e244e61
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-28 12:13:50 -0400

    Update questao_4.c

--- questao_4.c
+++ questao_4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+void dobrar (int *a) {
+	*a = *a * 2;
+}
+
+int main () {
+	int n1;
+
+	printf ("Digite um numero:\n");
+	scanf ("%d", &n1);
+
+	dobrar(&n1);
+
+	printf("O dobro é:%d\n", n1);
+
+	return 0;
+}
