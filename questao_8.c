f4d4dacd3d1563ac06cdb312ba392cc2b346b3ac
LeticiaBleik <leticiableikoliveira@gmail.com>
2026-05-29 08:38:24 -0400

    Update questao_8.c

--- questao_8.c
+++ questao_8.c
@@ -2,6 +2,7 @@
 
 void calculadora (float a, float b, float * soma, float * sub, float *multi, float *divisao) {
 
+	// operações
 	*soma = a + b;
 	*sub = a - b;
 	*multi = a * b;
@@ -17,6 +18,7 @@ int main () {
 
 	calculadora (n1, n2, &soma, &sub, &multi, &divisao);
 
+	// Coloquei vários floats porquê queria que deixar mais organizado no terminal passando a ideia de cálculo (Ex: 2+2 = 4)
 	printf("RESULTADO\n%.1f + %.1f = %.1f\n%.1f - %.1f = %.1f\n%.1f * %.1f = %.1f\n%.1f / %.1f = %.2f", n1,n2, soma, n1, n2, sub, n1, n2, multi, n1, n2, divisao);
 
 	return 0;
